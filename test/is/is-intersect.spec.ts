10c65c68ab9530326e36a01e0320a92c632bd607
Fidel Yiu <fidel-yiu@foxmail.com>
2022-04-12 21:38:21 +0800

    chore: 添加相交测试用例

--- test/is/is-intersect.spec.ts
+++ test/is/is-intersect.spec.ts
@@ -1,14 +1,16 @@
 import moment from "moment";
 import isIntersect from "../../src/is/is-intersect";
 
-test("isIntersect 时间是否相交", () => {
-    const x1 = "2022-03-12 00:00:00:000";
-    const x2 = "2022-05-13 00:00:00:000";
-    const x3 = "2022-04-15 08:00:00:00";
-    const x4 = "2022-04-18 08:00:00:000";
-
-    const format = "YYYY-MM-DD HH:mm:ss:SSS";
-    const result1 = isIntersect(moment(x1, format), moment(x2, format), moment(x3, format), moment(x4, format));
-    // console.log("result1", result1);
-    expect(result1).toBe(true);
+const format = "YYYY-MM-DD HH:mm:ss:SSS";
+describe.each([
+    { x1: "2022-03-12 00:00:00:000", x2: "2022-05-13 00:00:00:000", x3: "2022-04-15 08:00:00:00", x4: "2022-04-18 08:00:00:000", result: true },
+    { x1: "2022-04-11 00:00:00:000", x2: "2022-04-12 00:00:00:000", x3: "2022-04-12 08:00:00:000", x4: "2022-04-12 08:00:00:000", result: false },
+    { x1: "2022-04-12 00:00:00:000", x2: "2022-04-13 00:00:00:000", x3: "2022-04-12 08:00:00:000", x4: "2022-04-12 08:00:00:000", result: true },
+    { x1: "2022-04-11 00:00:00:000", x2: "2022-04-12 00:00:00:000", x3: "2022-04-12 00:00:00:000", x4: "2022-04-12 00:00:00:000", result: false },
+    { x1: "2022-04-12 00:00:00:000", x2: "2022-04-13 00:00:00:000", x3: "2022-04-12 00:00:00:000", x4: "2022-04-12 00:00:00:000", result: false },
+])("isIntersect: 相交测试", ({ x1, x2, x3, x4, result }) => {
+    it(`[${x1}, ${x2}, ${x3}, ${x4}]: 相交测试`, () => {
+        const data = isIntersect(moment(x1, format), moment(x2, format), moment(x3, format), moment(x4, format));
+        expect(data).toBe(result);
+    });
 });
