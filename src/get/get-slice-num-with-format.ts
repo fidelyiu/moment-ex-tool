e417a83f88c4dc3c0794cbce380ae4996a94acee
Fidel Yiu <fidel-yiu@foxmail.com>
2022-05-05 21:41:32 +0800

    chore: 修改切片计算类型

--- src/get/get-slice-num-with-format.ts
+++ src/get/get-slice-num-with-format.ts
@@ -1,10 +1,10 @@
 import getSliceTimeWithFormat from "./get-slice-time-with-format";
 
-import type { SliceNumOpt } from "../types";
+import type { SliceOpt } from "../types";
 
 /**
  * 判断`t1`、`t2`之间有多少时间切片数量，会自动对`t1`进行`startOf`处理，默认'd'。
  */
-export default function getSliceNumWithFormat(t1: moment.Moment, t2: moment.Moment, opt?: Partial<SliceNumOpt>): number {
+export default function getSliceNumWithFormat(t1: moment.Moment, t2: moment.Moment, opt?: Partial<SliceOpt>): number {
     return getSliceTimeWithFormat(t1, t2, opt).length;
 }
--- src/get/get-slice-num.ts
+++ src/get/get-slice-num.ts
@@ -1,10 +1,10 @@
 import getSliceTime from "./get-slice-time";
 
-import type { SliceNumOpt } from "../types";
+import type { SliceOpt } from "../types";
 
 /**
  * 判断`t1`、`t2`之间有多少时间切片数量
  */
-export default function getSliceNum(t1: moment.Moment, t2: moment.Moment, opt?: Partial<SliceNumOpt>): number {
+export default function getSliceNum(t1: moment.Moment, t2: moment.Moment, opt?: Partial<SliceOpt>): number {
     return getSliceTime(t1, t2, opt).length;
 }
